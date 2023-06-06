67e4fa5fb25067e78cb8ad044b2036b1ea943210
jiwon2724 <stopone3119@naver.com>
2023-06-06 16:03:26 +0900

    https://developer.android.com/codelabs/jetpack-compose-state?hl=ko&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%3Fhl%3Dko%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-state#0
    codelab-state 완료

--- compose-layout/src/main/java/com/jiwondev/compose_layout/MainActivity.kt
+++ compose-layout/src/main/java/com/jiwondev/compose_layout/MainActivity.kt
@@ -22,6 +22,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -33,6 +35,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -59,13 +62,39 @@ import java.util.Locale
  * **/
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Scaffold(bottomBar = { SootheBottomNavigation() }) { padding ->
+                HomeScreen(Modifier.padding(padding))
+            }
         }
     }
 }
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
@@ -134,7 +163,7 @@ fun AlignYourBodyRow(
         modifier = modifier
     ) {
         items(alignYourBodyData) { item ->
-            AlignYourBodyElement(drawable = item.drawble, text = item.text)
+            AlignYourBodyElement(drawable = item.drawable, text = item.text)
         }
     }
 }
@@ -169,25 +198,18 @@ fun FavoriteCollectionCard(
 }
 
 @Composable
-fun FavoriteCollectionsGrid(
-    modifier: Modifier = Modifier
-) {
-
-    LazyHorizontalGrid(
-        rows =,
-        content =
-    )
+fun FavoriteCollectionsGrid(modifier: Modifier = Modifier) {
     LazyHorizontalGrid(
         rows = GridCells.Fixed(2),
         contentPadding = PaddingValues(horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier
+        modifier = modifier.height(120.dp)
     ) {
-        items(favoriteCollectionsData) { item ->
+        items(alignYourBodyData) { item ->
             FavoriteCollectionCard(
-                item.drawable,
-                item.text,
+                drawable = item.drawable,
+                text = item.text,
                 modifier = Modifier.height(56.dp)
             )
         }
